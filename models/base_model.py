8a9772c7eef1c69c88cf9d0f5bf326291bd3477a
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-04 13:31:33 +0100

    fix(console)

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
@@ -8,15 +8,19 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
-        if not kwargs:  # if no dictionary (key&value) argument is passed
-            self.id = str(uuid4())
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            self.__set_from_dict(**kwargs)
-            storage.new(self)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -38,18 +42,3 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-
-    def __set_from_dict(self, **kwargs):
-        """Private method that creates a new instance from dict args"""
-        for k, v in kwargs.items():
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid4())
-            if k != "__class__":
-                if k in ("created_at", "updated_at"):
-                    # Construct a date from the output of date.isoformat()
-                    # setattr(x, 'y', v) is equivalent to x.y = v where;
-                    # x is dict_name, y is the key and v is the value
-                    setattr(self, k, datetime.fromisoformat(v))
-                # otherwise create a key and value pair of other attributes
-                else:
-                    setattr(self, k, v)
