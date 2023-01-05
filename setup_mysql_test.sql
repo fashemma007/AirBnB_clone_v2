5d3e68d34632e822a8afbc08079d4ddedb2284f2
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-05 08:09:02 +0100

    fix(sql)

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,14 +7,14 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 
 	IF NOT EXISTS 'hbnb_test'@'localhost' 
 	IDENTIFIED BY 'hbnb_test_pwd';
--- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost'
-GRANT ALL PRIVILEGES 
-	ON `hbnb_test_db`.* 
-	TO 'hbnb_test'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 -- hbnb_test should have SELECT privilege on the database performance_schema
 --  (and only this database)
 GRANT SELECT 
 	ON `performance_schema`.* 
+	TO 'hbnb_test'@'localhost';
+-- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+GRANT ALL PRIVILEGES 
+	ON `hbnb_test_db`.* 
 	TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
