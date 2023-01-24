cd37b8f3ff7c720e30985d0489db415b2a7cbc49
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-24 09:40:02 +0100

    remove comments

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,19 +13,12 @@ env.key_filename = "~/.ssh/school"
 def do_clean(number=0):
     """deletes out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
-    # Delete all unnecessary archives
-    # (all archives minus the number to keep) in the versions folder
     archives = sorted(os.listdir("versions"))
-    # print(archives)
     [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local(f"rm ./{archive}") for archive in archives]
-    # Delete all unnecessary archives
-    # (all archives minus the number to keep) in the
-    # /data/web_static/releases folder of both web servers
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [arch for arch in archives if "web_static_" in arch]
         [archives.pop() for i in range(number)]
-        # print(archives)
         [run("rm -rf ./{}".format(a)) for a in archives]
