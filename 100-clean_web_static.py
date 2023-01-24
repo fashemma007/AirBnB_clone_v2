53804eb5d5f1a3932e6938d57d5e8c0a8042e824
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-24 09:35:43 +0100

    hmmmm

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,12 +12,7 @@ env.key_filename = "~/.ssh/school"
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives
-    """
+    """delete out-of-date archives."""
     number = 1 if int(number) == 0 else int(number)
     # Delete all unnecessary archives
     # (all archives minus the number to keep) in the versions folder
