d4cdf1427f54f001fa20dbe3c03bcedc1794b2d8
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-05 10:41:55 +0100

    fix[url]: fix trailing slash error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
     """
     The hello_world function returns the string 'Hello HBNB!'
